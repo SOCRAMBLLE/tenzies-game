f54791b44a4be2709955e004278a45955c7ba47c
SOCRAMBLLE <mfragaprt@me.com>
2024-03-15 11:54:15 +0100

    Added 10 dices generated randomly and saved in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import Dice from "./components/Dice";
+
 function App() {
-  return <main></main>;
+  const [diceArray, setDiceArray] = useState(generateDiceArray());
+
+  function generateDiceArray() {
+    const randomDiceNumber = () => Math.floor(Math.random() * 6) + 1;
+    const randomDiceArray = Array.from({ length: 10 }, () =>
+      randomDiceNumber()
+    );
+    return randomDiceArray;
+  }
+  return (
+    <main>
+      <div className="dice-container">
+        {diceArray.map((dice, index) => (
+          <Dice key={index} value={dice} />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Dice.jsx
@@ -0,0 +1,4 @@
+/* eslint-disable react/prop-types */
+export default function Dice(props) {
+  return <button className="dice">{props.value}</button>;
+}
